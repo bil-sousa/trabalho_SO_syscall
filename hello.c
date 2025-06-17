bf4502d2c02a54a05308f56cc20256b87aab85f3
bil-sousa <153782558+bil-sousa@users.noreply.github.com>
2025-06-17 00:35:16 +0000

    dsg

---
+++ hello.c
@@ -0,0 +1,9 @@
+// Invocação de uma syscall (em C)
+//gcc -o hello hello.c
+#include <unistd.h>
+
+int main (int argc, char *argv[])
+{
+write (1, "Hello World!\n", 13) ; /* write string to stdout */
+exit (0) ; /* exit with no error */
+}
